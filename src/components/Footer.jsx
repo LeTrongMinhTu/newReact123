03729054c7c855c03b19834e2632700dd13dd22f
LeTrongMinhTu <letrongminhtu@gmail.com>
2025-12-30 10:35:58 +0700

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import "../css/Footer.css"
+function Footer() {
+    return (
+        <h2>Thank you!!!</h2>
+    );
+}
+export default Footer;
---
+++ src/components/Section2.jsx
@@ -0,0 +1,9 @@
+import "../css/Section2.css"
+function Section2 () {
+    return (
+        <section id="sec2">
+            <p>Parashaisifhebeichdnwewiissomf</p>
+        </section>
+    )
+}
+export default Section2;
